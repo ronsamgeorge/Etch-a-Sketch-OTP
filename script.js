8671b14a85b5fa2b41bcde3917bb0042b11a8a12
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-30 19:22:21 +0530

    Update css class names for better understanding

--- script.js
+++ script.js
@@ -5,31 +5,29 @@ function createGrid(gridSize=16){
 
     const container = document.querySelector('.container');
     
-   for(let i = 0; i<gridSize;i++){
+    for(let i = 0; i<gridSize;i++){
         const createDiv = document.createElement('div');
-        createDiv.classList.add("draw-border", "grid"+i);
+        createDiv.classList.add("grid", "g"+i);
         console.log(createDiv);
         container.appendChild(createDiv);
 
         createGridColumn(gridSize,i);
     }
+}
 
-    function createGridColumn(Rows,i){
+function createGridColumn(Rows,i){
 
-        const gridColumn = document.querySelector(".grid"+i);
-        
-        for(let j = 0; j<Rows; j++){
-            const createDiv = document.createElement('div');
-            createDiv.classList.add("draw-border", "row"+i);
-            console.log(createDiv);
-            gridColumn.appendChild(createDiv);
+    const gridColumn = document.querySelector(".g"+i);
     
-        }
-    }
+    for(let j = 0; j<Rows; j++){
+        const createDiv = document.createElement('div');
+        createDiv.classList.add("row", "row"+i+j);
+        console.log(createDiv);
+        gridColumn.appendChild(createDiv);
 
+    }
 }
 
 
-
-createGrid();
+createGrid(32);
 
