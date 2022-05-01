d93093ea08c2174a6b3823ed0824100506605303
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-02 00:28:42 +0530

    Add feature Change Grid Size and include hoverCells query selector in GridCreation()

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function createGrid(gridSize=16){
 
         createGridColumn(gridSize,i);
     }
+
+    //to get the new DOM cells
+    const hoveredCells = document.querySelectorAll(".row");
+    hoveredCells.forEach(cell => cell.addEventListener('mouseover',onHover));
 }
 
 function createGridColumn(Rows,i){
@@ -29,18 +33,54 @@ function createGridColumn(Rows,i){
 
 
 function onHover(e){
-    console.log(this.id);
+    //console.log(this.id);
     const targetDivId = this.id;
     const targetDiv = document.querySelector('#'+targetDivId);
     targetDiv.classList.add('row-hover');
 }
 
+
+//onReset will clear the canvas when reset button is clicked. By removing class
 function onReset(e){
+
+    const allCells = document.querySelectorAll(".row");
+
+   console.log(allCells);
     
 }
 
-createGrid(32);
-const hoveredCells = document.querySelectorAll(".row");
+function onChangeGrid(e){
+    const newGridSize = prompt("Enter new Grid Size(<=100)");
+    console.log(newGridSize);
 
-hoveredCells.forEach(cell => cell.addEventListener('mouseover',onHover));
+    if(newGridSize > 100){
+        alert("Grid Size can't be more than 100.");
+        return;
+    }
+
+    const gridContainer = document.querySelector('.container');
+
+    while(gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(newGridSize);
+    return;
+}
+
+createGrid();
+
+
+/* Initially the hoverevent was handled here but since has been tranferred to the
+createGrid function because each time the grid is changed, we need to update the NodeList as well
+
+const hoveredCells = document.querySelectorAll(".row");
+hoveredCells.forEach(cell => cell.addEventListener('click',onHover));*/
+
+const resetButton = document.querySelector(".reset-btn");
+resetButton.addEventListener('click',onReset);
+
+const gridChange = document.querySelector(".change-grid-btn");
+gridChange.addEventListener('click', onChangeGrid);
+
+
 
